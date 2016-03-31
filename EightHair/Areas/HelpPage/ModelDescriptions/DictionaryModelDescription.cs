5e469589c9e24a551736b87734d8a1e65e627829
Natanael Lemos <natanaeltlemos@gmail.com>
2016-03-30 23:07:50 -0300

    Ops

--- EightHair/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace EightHair.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- EightHair/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EightHair.WebApiApplication" Language="C#" %>
